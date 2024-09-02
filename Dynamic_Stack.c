ee8cc07b24fe30f17a48c95c01e07ff6819ddc2d
asteroid <hwang9973@dgu.ac.kr>
2024-09-02 21:42:28 +0900

    start

---
+++ Dynamic_Stack.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STACK_SIZE 10000
+
+typedef int element;
+typedef struct {
+    element data[MAX_STACK_SIZE];
+    int top;
+} Stack;
+
+void init_stack(Stack* s);
+void push(Stack* s, element item);
+element pop(Stack* s);
+element top(Stack* s);
+int is_empty(Stack* s);
+int is_full(Stack* s);
+int size(Stack* s);
+
+void init_stack(Stack* s) {
+    s->top = -1;
+}
+
+void push(Stack* s, element item) {
+    if (is_full(s)) printf("Stack is full\n");
+    else s->data[++(s->top)] = item;
+}
+
+element pop(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty\n");
+        exit(1);
+    }
+    else return s->data[(s->top)--];
+}
+
+element top(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty\n");
+        exit(1);
+    }
+    else return s->data[s->top];
+}
+
+int is_empty(Stack* s) {
+    return (s->top == -1);
+}
+
+int is_full(Stack* s) {
+    return (s->top == (MAX_STACK_SIZE - 1));
+}
+
+int size(Stack* s) {
+    if (is_empty(s)) return 0;
+    else return s->top + 1;
+}
---
+++ Static_Stack.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct {
+    element* data;
+    int capacity;
+    int top;
+} Stack;
+
+void init_stack(Stack* s);
+void push(Stack* s, element item);
+element pop(Stack* s);
+element top(Stack* s);
+int is_empty(Stack* s);
+int is_full(Stack* s);
+int size(Stack* s);
+
+void init_stack(Stack* s) {
+    s->top = -1;
+    s->capacity = 1;
+    s->data = (element*)malloc(s->capacity * sizeof(element));
+}
+
+void push(Stack* s, element item) {
+    if (is_full(s)) {
+        s->capacity += 1;
+        s->data = (element*)realloc(s->data, s->capacity * sizeof(element));
+    }
+    s->data[++(s->top)] = item;
+}
+
+element pop(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty\n");
+        exit(1);
+    }
+    else return s->data[(s->top)--];
+}
+
+element top(Stack* s) {
+    if (is_empty(s)) {
+        printf("Stack is empty\n");
+        exit(1);
+    }
+    else return s->data[s->top];
+}
+
+int is_empty(Stack* s) {
+    return (s->top == -1);
+}
+
+int is_full(Stack* s) {
+    return (s->top == (s->capacity - 1));
+}
+
+int size(Stack* s) {
+    if (is_empty(s)) return 0;
+    else return s->top + 1;
+}
