d004c5224813b0eb0d309fa6bbdae7a36b8186ae
asteroid <hwang9973@dgu.ac.kr>
2024-11-18 20:38:33 +0900

    add_bfs/dfs

--- Graph/GraphAdjacencyList.c
+++ Graph/GraphAdjacencyList.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_VERTICES 50
+#define QUEUE_SIZE 100
 
 typedef struct Node {
 	int vertex;
@@ -12,7 +13,34 @@ typedef struct Graph {
 	Node* adj_list[MAX_VERTICES];
 } Graph;
 
-void init(Graph* g) {
+typedef int element;
+typedef struct {
+    element data[QUEUE_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void init_queue(Queue* q) {
+    q->front = q->rear = 0;
+}
+
+void enqueue(Queue* q, element item) {
+    if ((q->rear + 1) % QUEUE_SIZE == q->front) {
+        return;
+    }
+    q->rear = (q->rear + 1) % QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+element dequeue(Queue* q) {
+    if (q->front == q->rear) {
+        return NULL;
+    }
+    q->front = (q->front + 1) % QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+void init_graph(Graph* g) {
 	g->n = 0;
 	for (int v = 0; v < MAX_VERTICES; v++) {
 		g->adj_list[v] = NULL;
@@ -41,4 +69,49 @@ void insert_edge(Graph* g, int start, int end) {
     newNode->vertex = start;
     newNode->next = g->adj_list[end];
     g->adj_list[end] = newNode;
+}
+
+void DFS_recursive(Graph* g, int v, int* visited) {
+    Node* w;
+    visited[v] = 1;
+    printf("vertex %d -> ", v);
+    for (w = g->adj_list[v]; w; w = w->next) {
+        if (!visited[w->vertex]) {
+            DFS_recursive(g, w->vertex, visited);
+        }
+    }
+}
+
+void DFS_list(Graph* g, int v) {
+    int* visited = (int*)malloc(sizeof(int) * g->n);
+    for (int i = 0; i < g->n; i++) {
+        visited[i] = 0;
+    }
+
+    DFS_recursive(g, v, visited);
+    free(visited);
+}
+
+void BFS_list(Graph* g, int v) {
+    Node* w;
+    Queue q;
+    init_queue(&q);
+    int* visited = (int*)malloc(sizeof(int)*g->n);
+    for (int i = 0; i < g->n; i++) {
+        visited[i] = 0;
+    }
+    visited[v] = 1;
+    printf("%d visited -> ", v);
+    enqueue(&q, v);
+    while (!is_empty(&q)) {
+        v = dequeue(&q);
+        for (w = g->adj_list[v]; w; w = w->next) {
+            if (!visited[w->vertex]) {
+                visited[w->vertex] = 1;
+                printf("%d visited -> ", w->vertex);
+                enqueue(&q, w->vertex);
+            }
+        }
+    }
+    free(visited);
 }
--- Graph/GraphAdjacentMatrix.c
+++ Graph/GraphAdjacentMatrix.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_VERTICES 50
+#define QUEUE_SIZE 100
 
 typedef struct {
     int n;
     int adj_mat[MAX_VERTICES][MAX_VERTICES];
 } Graph;
 
-void init(Graph* g) {
+typedef int element;
+typedef struct {
+    element data[QUEUE_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void init_queue(Queue* q) {
+    q->front = q->rear = 0;
+}
+
+void enqueue(Queue* q, element item) {
+    if ((q->rear + 1) % QUEUE_SIZE == q->front) {
+        return;
+    }
+    q->rear = (q->rear + 1) % QUEUE_SIZE;
+    q->data[q->rear] = item;
+}
+
+element dequeue(Queue* q) {
+    if (q->front == q->rear) {
+        return NULL;
+    }
+    q->front = (q->front + 1) % QUEUE_SIZE;
+    return q->data[q->front];
+}
+
+void init_graph(Graph* g) {
     int r, c;
     g->n = 0;
     for (r = 0; r<MAX_VERTICES; r++) {
@@ -32,4 +60,47 @@ void insert_edge(Graph* g, int start, int end) {
     }
     g->adj_mat[start][end] = 1;
     g->adj_mat[end][start] = 1;
+}
+
+void DFS_recursive(Graph* g, int v, int* visited) {
+    int w;
+    visited[v] = 1;
+    printf("vertex %d -> ", v);
+    for (w = 0; w < g->n; w++) {
+        if (g->adj_mat[v][w] && !visited[w]) {
+            DFS_recursive(g, w, visited);
+        }
+    }
+}
+
+void DFS_mat(Graph* g, int v) {
+    int* visited = (int*)malloc(sizeof(int)*g->n);
+    for (int i = 0; i < g->n; i++) {
+        visited[i] = 0;
+    }
+    DFS_recursive(g, v, visited);
+    free(visited);
+}
+
+void BFS_mat(Graph* g, int v) {
+    int w;
+    Queue q;
+    init_queue(&q);
+    int* visited = (int*)malloc(sizeof(int)*g->n);
+    for (int i = 0; i < g->n; i++) {
+        visited[i] = 0;
+    }
+    visited[v] = 1;
+    printf("%d visited -> ", v);
+    enqueue(&q, v);
+    while (!is_empty(&q)) {
+        v = dequeue(&q);
+        for (w = 0; w<g->n; w++)
+        if (g->adj_mat[v][w] && !visited[w]) {
+            visited[w] = 1;
+            printf("%d visited -> ", w);
+            enqueue(&q, w);
+        }
+    }
+    free(visited);
 }
